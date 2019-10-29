caa657b2e049e78f1142c3ac766ebc76f85f9478
Ankit Agrawal <ankitagrawal11b@gmail.com>
2019-10-29 22:13:42 +0530

    Login Logout Added

--- file1.py
+++ file1.py
@@ -4,10 +4,12 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys 
 from selenium.webdriver.common.by import By 
 import time 
-
+from mods import login, logout
 
 driver = webdriver.Chrome('chromedriver.exe')
 driver.minimize_window()
+
+login(driver)
 
 # marks fetching link
 url = "https://www.hackerrank.com/contests/hacktime-1/challenges"
@@ -17,4 +19,6 @@ driver.get(url)
 
 wait = WebDriverWait(driver, 1200)
 time.sleep(3)
+
+logout(driver)
 driver.close()
