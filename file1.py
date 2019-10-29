dddf3aa249f0cbd362411b8351babe5c08ce2741
Ankit Agrawal <ankitagrawal11b@gmail.com>
2019-10-29 22:35:56 +0530

    Scoreboard fetched

--- file1.py
+++ file1.py
@@ -19,6 +19,27 @@ url = "https://www.hackerrank.com/contests/"+ input("Enter Contest Name") +"/cha
 
 driver.get(url)
 
+
+student_scoreboard = dict()
+# new_driver = webdriver.Chrome("chromedriver.exe")
+# login(new_driver)
+
+# max_score = getMaxScore(driver)
+
+href = driver.find_element_by_class_name("w_challenges_side_list").find_element_by_class_name("backbone").get_attribute("href")
+
+driver.get(href)
+time.sleep(7)
+
+t_leaders = driver.find_element_by_id("leaders")
+leaders = t_leaders.find_elements_by_class_name("span-flex-4")
+for j in range(len(leaders)):
+    leader = leaders[j].find_element_by_class_name("leaderboard-hackername")
+    leader_name = leader.get_attribute("data-attr1")
+    leader_score = t_leaders.find_elements_by_class_name("span-flex-3")[2*j].find_element_by_tag_name("p").text
+    student_scoreboard[leader_name] = student_scoreboard.get(leader_name,0)+float(leader_score)
+
+
 wait = WebDriverWait(driver, 1200)
 time.sleep(3)
 
